935f243e67fc24839793658a9614ee4b5ac9d711
Hasnain mawia <hasnainmawia.w@gmail.com>
2025-05-06 09:55:14 +0500

    Footer added

--- index.php
+++ index.php
@@ -244,6 +244,9 @@
       </div>
     </div>
 
+    <!-- -------------------- Testimonail [End] ------------------------ -->
+
+    <!-- -------------------- Reach [Start] ------------------------ -->
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">Reach Us</h2>
     <div class="container">
       <div class="row">
@@ -262,17 +265,37 @@
         </div>
       </div>
     </div>
+    <!-- -------------------- Reach [End] ------------------------ -->
 
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <!-- -------------------- Testimonail [End] ------------------------ -->
+    <!-- -------------------- Footer [Start] ------------------------ -->
+    <footer class="container ">
+          <div class="row">
+            <div class="col-lg-4 p-4">
+              <h3 class="h-font fw-bold fs-3 mb-2">TJ Hotel</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non quae et tenetur rem quod, distinctio aliquid voluptas, voluptates quam culpa eveniet iure enim doloribus quia dolorem porro, eum magni provident!</p>
+            </div>
+            <div class="col-lg-4 p-4">
+              <h5 class="mb-3">Links</h5>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class="col-lg-4 p-4">
+              <h5 class="mb-3">Follow us</h5>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-facebook fs-5"></i> Facebook</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-instagram fs-5"></i> Instagram</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-linkedin fs-5"></i></i> Linkedin</a><br>
+              <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none"><i class="bi bi-twitter-x fs-5"></i> Twitter</a><br>
+
+            </div>
+          </div>
+    
+    </footer>
+ 
+    <!-- -------------------- Footer [End] ------------------------ -->
 
     <!-- -------------------- Login Modal [Start] ------------------------ -->
      <!-- Modal -->
